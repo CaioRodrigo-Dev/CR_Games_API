32f290766e2ebed6c966a622d83b4e5ac9e397f9
Caio Rodrigo <caiorodrigo_dev@outlook.com>
2025-01-07 19:17:21 -0300

    Adding JwtService.

---
+++ CR_Games_API - Service/Auth/JwtService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR_Games_API___Service.Auth
+{
+    public class JwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(string userId, string login)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+
+            new Claim(JwtRegisteredClaimNames.Sub, userId),
+            new Claim(JwtRegisteredClaimNames.Email, login),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+         };
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["ExpiresInMinutes"])),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- CR_Games_API/Program.cs
+++ CR_Games_API/Program.cs
@@ -4,6 +4,7 @@ using CR_Games_API___Infra;
 using CR_Games_API___Infra.DBContext;
 using CR_Games_API___Infra.Repoitory.Interfaces;
 using CR_Games_API___Infra.Repoitory;
+using CR_Games_API___Service.Auth;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +15,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddSingleton<JwtService>();
 #region Repository DI
 builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
 #endregion

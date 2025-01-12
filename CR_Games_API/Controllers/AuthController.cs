993744176e5bc68154fa99fd3355f6ad098d0e8d
Caio Rodrigo <caiorodrigo_dev@outlook.com>
2025-01-11 22:25:38 -0300

    Adding AuthController and Login endpoint.

---
+++ CR_Games_API/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CR_Games_API.Controllers
+{
+    [ApiController]
+    [Route("Controller")]
+
+    public class AuthController : Controller
+    {
+        #region Fields
+        private readonly IAuthService _authService;
+        #endregion
+
+        #region Constructor
+        public AuthController (IAuthenticationService authService)
+        {
+            _authService = authService;
+        }
+        #endregion
+
+        #region EndPoints
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] AuthLoginRequestDTO request)
+        {
+            try
+            {
+                var result = await _authService.Authenticate(request);
+                return new JsonResult(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("Logout")]
+        public IActionResult Logout()
+        {
+            return Ok(new { message = "Sessão encerrada." });
+        }
+        #endregion
+    }
+}
